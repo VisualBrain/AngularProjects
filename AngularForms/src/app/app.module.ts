380c3fa9d4ec4db900afd8e42f84d1296f6ce97d
Chaurasia <765406@cognizant.com>
2019-09-11 12:42:09 +0530

    Angular Forms Completed

--- AngularForms/src/app/app.module.ts
+++ AngularForms/src/app/app.module.ts
@@ -3,9 +3,13 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import {FormsModule} from '@angular/forms';
-import {HttpClientModule} from '@angular/common/http';
+import { FormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
 import { UserFormSettingsComponent } from './user-form-settings/user-form-settings.component';
+import { ButtonsModule } from 'ngx-bootstrap/buttons';
+import { BsDatepickerModule } from 'ngx-bootstrap/datepicker';
+import { TimepickerModule } from 'ngx-bootstrap/timepicker';
+import { RatingModule } from 'ngx-bootstrap/rating';
 
 @NgModule({
   declarations: [
@@ -16,7 +20,11 @@ import { UserFormSettingsComponent } from './user-form-settings/user-form-settin
     BrowserModule,
     AppRoutingModule,
     FormsModule,
-    HttpClientModule
+    HttpClientModule,
+    ButtonsModule.forRoot(),
+    BsDatepickerModule.forRoot(),
+    TimepickerModule.forRoot(),
+    RatingModule.forRoot()
   ],
   providers: [],
   bootstrap: [AppComponent]
--- AngularForms/src/app/user-form-settings/user-form-settings.component.ts
+++ AngularForms/src/app/user-form-settings/user-form-settings.component.ts
@@ -2,7 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { UserSettings } from '../data/user-settings';
 import { NgForm, NgModel } from '@angular/forms';
 import { DataService } from '../data/data.service';
-import {Observable} from 'rxjs';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-user-form-settings',
@@ -25,10 +25,17 @@ export class UserFormSettingsComponent implements OnInit {
   postErrorMessage = '';
   // subscriptionTypes: string[] = ['Monthly', 'Annual', 'LifeTime'];
   subscriptionTypes: Observable<string[]>;
+  singleModel: string = 'On';
   constructor(private dataService: DataService) { }
-
+  startDate:Date;
+  rangeDate:Date;
+  startTime:Date;
+  userRating = 0;
+  maxRating = 10;
   ngOnInit() {
     this.subscriptionTypes = this.dataService.getSubscriptionTypes();
+    this.startDate = new Date();
+    this.startTime = new Date();
   }
 
   /*
@@ -58,25 +65,34 @@ export class UserFormSettingsComponent implements OnInit {
     );
   }
   */
- // refer image: changes-034
- onSubmit = (form: NgForm) => {
-  console.log('In Submit:', form.valid);
-  if (form.valid) {
-    this.dataService.postUserSettingsForm(this.userSettings).subscribe(
-      result => { console.log('Success:', result); },
-      error => { this.onHttpError(error); }
-    );
-  } else {
-    this.postError = true;
-    this.postErrorMessage = 'Please Fix The above Error';
+  // refer image: changes-034
+  /*
+  onSubmit = (form: NgForm) => {
+    console.log('In Submit:', form.valid);
+    if (form.valid) {
+      this.dataService.postUserSettingsForm(this.userSettings).subscribe(
+        result => { console.log('Success:', result); localStorage.setItem('result',JSON.stringify(result));document.cookie = "username=Angular Forms; expires=Monday,12 September 2019 12:00:00 UTC; path=/";
+        sessionStorage.setItem('result',result.subscriptionType);},
+        error => { this.onHttpError(error); }
+      );
+    } else {
+      this.postError = true;
+      this.postErrorMessage = 'Please Fix The above Error';
+    }
   }
-}
   onHttpError = (errorResponse: any) => {
     console.log('Error : ', errorResponse);
     this.postError = true;
     this.postErrorMessage = errorResponse.error.errorMessage;
   }
+   */
   onBlur = (nameField: NgModel) => {
     console.log('In Blur:', nameField.valid);
+    console.log(localStorage.getItem('result'));
+    console.log(sessionStorage.getItem('result'));
+  }
+ 
+  onSubmit = (form: NgForm) => {
+    console.log('In Submit:', form.value);
   }
 }
